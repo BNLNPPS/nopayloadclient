f20699c46c6d6e58fd716393e8a1fb13e50bd939
ligerlac <lino.oscar.gerlach@cern.ch>
2022-10-17 15:48:00 +0200

    wrote cmake file for examples folder

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "../include/nopayloadclient.hpp"
+#include "nopayloadclient.hpp"
 
 int main()
 {
--- tests/test_nopayloadclient.cpp
+++ tests/test_nopayloadclient.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include "plmover.hpp"
 #include "nopayloadclient.hpp"
 
 int main()
