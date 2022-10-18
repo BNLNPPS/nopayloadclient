c2e5ce08291c236d8738e7dd0a8df4e0ad87174a
ligerlac <lino.oscar.gerlach@cern.ch>
2022-10-18 11:42:15 +0200

    finalized config search path set up

--- examples/example.cpp
+++ examples/example.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include "nopayloadclient.hpp"
+#include "config.hpp"
 
 int main()
 {
   std::cout<<"Hello, world!"<<std::endl;
-  //  nopayloadclient::get("my_gt", "my_pl_type", 1);
+  std::cout<<config::rawDict()<<std::endl;
+  
+  nopayloadclient::insertPayload("my_gt", "my_pl_type",
+   "/Users/linogerlach/Projects/DUNE/ConditionsHandling/nopayloadclient/data/prompt/data.dat", 0);
+
+  //std::cout<<"NOPAYLOADCLIENT_DATA_INSTALL_DIR_FULL = "<<std::endl;
+  //std::cout<<std::getenv("@NOPAYLOADCLIENT_DATA_INSTALL_DIR_FULL@")<<std::endl;
+  //nopayloadclient::get("my_gt", "my_pl_type", 1);
   return 0;
 }
--- src/config.cpp
+++ src/config.cpp
@@ -2,15 +2,23 @@
 #include <fstream>
 #include <nlohmann/json.hpp>
 
+#include "config.h"
+
 
 namespace config {
 
 nlohmann::json _rawDict;
 
-nlohmann::json fromFile(std::string filePath){
-    std::cout<<"fromFile(filePath="<<filePath<<")"<<std::endl;
+nlohmann::json fromFile(std::string fileName){
+    std::cout<<"fromFile(fileName="<<fileName<<")"<<std::endl;
+    std::cout<<"INSTALL_CONFIG_SEARCH_PATHS ="<<std::endl;
+    std::cout<<INSTALL_CONFIG_SEARCH_PATHS<<std::endl;
+    std::cout<<"PROJECT_CONFIG_SEARCH_PATHS ="<<std::endl;
+    std::cout<<PROJECT_CONFIG_SEARCH_PATHS<<std::endl;
+    std::string fullPath = PROJECT_CONFIG_SEARCH_PATHS + fileName;
+    std::cout<<"fullPath = "<<fullPath<<std::endl;
+    std::ifstream conf_file(fullPath, std::ifstream::binary);
     nlohmann::json j;
-    std::ifstream conf_file(filePath, std::ifstream::binary);
     conf_file >> j;
     return j;
 }
@@ -19,7 +27,7 @@ nlohmann::json fromFile(){
     char* confPath = std::getenv("NOPAYLOADCLIENT_CONF");
     if (confPath==NULL){
         std::cout<<"variable NOPAYLOADCLIENT_CONF not set. Using default.json..."<<std::endl;
-        return fromFile("../../config/default.json");
+        return fromFile("default.json");
     }
     return fromFile(confPath);
 }
