23f6e977459297c5906d7825a26626ed22da48ba
ligerlac <lino.oscar.gerlach@cern.ch>
2022-10-31 06:50:46 -0400

    removed automatically generated file from git

--- src/config.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define INSTALL_CONFIG_SEARCH_PATHS "/usr/local/share/nopayloadclient/config/"
-#define PROJECT_CONFIG_SEARCH_PATHS "/Users/linogerlach/Projects/DUNE/ConditionsHandling/nopayloadclient/config/"
