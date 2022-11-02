e1f575c684bcaeeb5d246b74785a025d6221a729
ligerlac <lino.oscar.gerlach@cern.ch>
2022-11-02 14:27:40 +0100

    simplified project structure: removed intercace files

--- include/nopayloadclient.hpp
+++ include/nopayloadclient.hpp
@@ -1,3 +1,15 @@
 #pragma once
 
-#include "interface.hpp"
+#include <nlohmann/json.hpp>
+
+namespace nopayloadclient {
+    // Reading
+    nlohmann::json get(std::string gtName, std::string plType, int majorIov, int minorIov);
+
+    // Writing
+    nlohmann::json createPayloadType(std::string plType);
+    nlohmann::json createGlobalTag(std::string gtName);
+    nlohmann::json lockGlobalTag(std::string gtName);
+    nlohmann::json insertPayload(std::string gtName, std::string plType, std::string fileUrl,
+                                 int majorIovStart, int minorIovStart);
+}
--- tests/test_get.cpp
+++ tests/test_get.cpp
@@ -6,10 +6,8 @@
 
 int main()
 {
-  std::cout<<"running test_get()..."<<std::endl;
-//  std::cout<<"config::dict = "<<config::dict<<std::endl;
-  //std::cout<<"config::api_url = "<<config::api_url<<std::endl;
   nlohmann::json resp = nopayloadclient::get("my_gt", "my_pt", 1410065408, 0);
-  //std::cout<<resp["msg"]<<std::endl;
+  std::cout<<resp<<std::endl;
+
   return 0;
 }
