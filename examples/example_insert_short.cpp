8f8c98a3e3d7e5b47b286143504fae53b48f3053
ligerlac <lino.oscar.gerlach@cern.ch>
2022-11-03 14:48:25 +0100

    prettified insertion example

--- examples/example_insert_short.cpp
+++ examples/example_insert_short.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
-#include <nlohmann/json.hpp>
+#include <nlohmann/json.hpp> // nlohman json object is returned by every nopayloadclient function
 #include "nopayloadclient.hpp"
 
 
 int main()
 {
+  // local payload files are specified by absolute path. this variable is just for convenience
   std::string basePath = "/Users/linogerlach/Projects/DUNE/ConditionsHandling/nopayloadclient/data/local/";
   //std::string basePath = "/lbne/u/lgerlach1/Projects/nopayloadclient/data/local/";
 
+  // create json object to which the response is written
   nlohmann::json resp;
+
+  // try inserting payload
   resp = nopayloadclient::insertPayload("sPHENIX_ExampleGT_1", "Beam", basePath+"D0DXMagnets.dat", 0, 0);
-  std::cout<<resp<<std::endl;
-  resp = nopayloadclient::insertPayload("sPHENIX_ExampleGT_1", "Beam", basePath+"D0DXMagnets.dat", 0, 0);
-  std::cout<<resp<<std::endl;
-  /*
-  resp = nopayloadclient::insertPayload("sPHENIX_ExampleGT_1", "FieldMap", basePath+"sphenix3dbigmapxyz.root", 0, 0);
-  std::cout<<resp<<std::endl;
-  resp = nopayloadclient::insertPayload("sPHENIX_ExampleGT_1", "CEMC_Thresh", basePath+"CEMCprof_Thresh30MeV.root", 0, 0);
-  std::cout<<resp<<std::endl;
-  resp = nopayloadclient::insertPayload("sPHENIX_ExampleGT_1", "CEMC_Geo", basePath+"cemc_geoparams-0-0-4294967295-1536789215.xml", 0, 0);
-  std::cout<<resp<<std::endl;
-  */
+
+  // analyse response: code==0 -> OK, code!=0 -> Not OK!
+  if (resp["code"]!=0) {
+    std::cout<<"something went wrong. error message: "<<resp["msg"]<<std::endl;
+  }
+  else {
+    std::cout<<"all good in da hood. details: "<<resp["msg"]<<std::endl;
+  }
+
+  // w/o error handling, inserting a payload is a one-liner:
+  nopayloadclient::insertPayload("sPHENIX_ExampleGT_1", "Beam", basePath+"D0DXMagnets.dat", 0, 0);
+  nopayloadclient::insertPayload("sPHENIX_ExampleGT_1", "FieldMap", basePath+"sphenix3dbigmapxyz.root", 0, 0);
+  nopayloadclient::insertPayload("sPHENIX_ExampleGT_1", "CEMC_Thresh", basePath+"CEMCprof_Thresh30MeV.root", 0, 0);
+  nopayloadclient::insertPayload("sPHENIX_ExampleGT_1", "CEMC_Geo", basePath+"cemc_geoparams-0-0-4294967295-1536789215.xml", 0, 0);
+
   return 0;
 }
 
