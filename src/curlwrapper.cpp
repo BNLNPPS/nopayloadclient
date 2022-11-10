c3a355399080da770c2d12fb14fcb07c89e0290b
ligerlac <lino.oscar.gerlach@cern.ch>
2022-11-10 16:44:39 +0100

    added time measurement around curl call

--- src/curlwrapper.cpp
+++ src/curlwrapper.cpp
@@ -56,7 +56,11 @@ class CurlMession{
         }
 
         nlohmann::json execute(){
+            std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
             ans.res = curl_easy_perform(curl);
+            std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+            int n_mu_s = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
+            std::cout << "Time difference = " <<double(n_mu_s)/1000000 << "[s]" << std::endl;
             if ( ans.res!=0 ){
                 std::string const msg = "curl_easy_perform() failed with error code: " + std::to_string(ans.res);
                 throw std::runtime_error(msg);
@@ -66,7 +70,7 @@ class CurlMession{
                 throw NoPayloadException(ans.readBuffer);
             }
             curl_easy_cleanup(curl);
-            printResults();
+            //printResults();
             return nlohmann::json::parse(ans.readBuffer);
         }
 
