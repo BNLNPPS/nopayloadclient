75f73de8268bbd9582ea10f69db429f0da8e0c89
ligerlac <lino.oscar.gerlach@cern.ch>
2023-01-19 12:40:49 +0100

    extracted more CI into scripts

---
+++ scripts/prepare_ssh.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Attempting to prepare ssh."
+read -p "This will fiddle with the ssh config. Continue (y/n)?" CONT
+if [ "$CONT" != "y" ]; then
+    echo "user did not confirm. exiting...";
+    exit 0
+fi
+
+mkdir -p ~/.ssh
+ssh-keyscan github.com >> ~/.ssh/known_hosts
+ssh-agent -a $SSH_AUTH_SOCK > /dev/null
+ssh-add - <<< "${{ secrets.DEPLOY_KEY }}"
