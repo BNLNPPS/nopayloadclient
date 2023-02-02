6cebcca39685118af25f1b301ac6406cef5d351b
ligerlac <lino.oscar.gerlach@cern.ch>
2023-02-02 18:58:26 +0100

    removed deprecated code

--- include/nopayloadclient/curlwrapper.hpp
+++ include/nopayloadclient/curlwrapper.hpp
@@ -11,25 +11,6 @@
 #include <nopayloadclient/exception.hpp>
 
 
-namespace curlwrapper {
-
-struct Answer {
-  CURLcode res;
-  std::string readBuffer;
-  long httpCode = 0;
-};
-
-// Reading
-nlohmann::json get(std::string url);
-
-// Writing
-nlohmann::json del(std::string url);
-nlohmann::json put(std::string url);
-nlohmann::json put(std::string url, nlohmann::json jsonData);
-nlohmann::json post(std::string url, nlohmann::json jsonData);
-
-}
-
 class CurlWrapper {
 public:
     CurlWrapper(const nlohmann::json& config);
