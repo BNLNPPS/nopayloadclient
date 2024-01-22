c53df63a3b1306f8875595e0b41baf7a0fa898c3
ligerlac <lino.oscar.gerlach@cern.ch>
2024-01-22 22:35:09 +0100

    implement new sleep time

--- src/realwrapper.cpp
+++ src/realwrapper.cpp
@@ -7,10 +7,12 @@ RealWrapper::RealWrapper(const json& config) {
     base_url_ += config["base_url"];
     base_url_ += config["api_res"];
     n_retries_ = config["n_retries"];
+    retry_sleep_mean_ = config["retry_sleep_mean"];
 }
 
 void RealWrapper::sleep(int retry_number) {
-    int n_sleep = int(std::exp(retry_number));
+    srand(time(0));
+    int n_sleep = rand()%(retry_sleep_mean_*2)+1; // add random sleep
     logging::debug("sleeping for " + std::to_string(n_sleep) + " seconds before retrying...");
     std::this_thread::sleep_for(std::chrono::seconds(n_sleep));
 }
