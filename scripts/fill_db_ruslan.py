2fcb7f55dad554f5e9c78d6adbf9ac4e37b25ef8
ruslan33 <rmashinistov@gmail.com>
2024-04-29 01:36:40 +0200

    Update fill_db_ruslan.py. Add JWT and fix APIs

--- scripts/fill_db_ruslan.py
+++ scripts/fill_db_ruslan.py
@@ -1,4 +1,12 @@
 import requests
+
+# Your JWT
+jwt_token = 'your_jwt_token_here'
+
+# Headers with Authorization Bearer token
+headers = {
+    'Authorization': f'Bearer {jwt_token}'
+}
   
 # Payload Types are user-defined, examples from Chris
 PL_TYPEs = ['Beam', 'FieldMap', 'CEMC_Thresh', 'ZDC', 'CEMC_Geo']
@@ -16,35 +24,14 @@ GT_NAME = "sPHENIX_ExampleGT_1"
 
 base_url = "http://127.0.0.1:8000/api/cdb_rest"
 
-# Create a global tag type object - this should be a VERY rare occurence
-print("Create Global Tag Type")
-gttype = {'name':'testtype'} # id should be autoincr, name should be unique
-url = base_url + '/gttype'
-r = requests.post(url = url, json=gttype)
-print(r.json())
-
-
-# Create a global tag status object - this should be a VERY rare occurence
-print("Create Global Tag Status")
-gtstatus = {'name':'locked'} # id should be autoincr, name should be unique
-url = base_url + '/gtstatus'
-r = requests.post(url = url, json=gtstatus)
-print(r.json())
-gtstatus = {'name':'unlocked'} # id should be autoincr, name should be unique
-url = base_url + '/gtstatus'
-r = requests.post(url = url, json=gtstatus)
-print(r.json())
-
-
 # Create a global tag object - this should be QUITE rare
 print("Create Global Tag Object")
 gt = {
     'name': GT_NAME,
-    'status': "unlocked",
-    'type': "testtype"
+    'status': "unlocked"
 }
 url = base_url + '/gt'
-r = requests.post(url = url, json=gt)
+r = requests.post(url = url, headers=headers, json=gt)
 print(r.json())
 
 
@@ -53,7 +40,7 @@ print("Create Payload Types")
 url = base_url + '/pt'
 for pl_type in PL_TYPEs:
     pt = {'name':pl_type} # id should be autoincr, name should be unique
-    r = requests.post(url = url, json=pt)
+    r = requests.post(url = url, headers=headers, json=pt)
     print(r.json())
 
 # Create payload_list objects (based only on payload_types) - this is QUITE rare
@@ -64,7 +51,7 @@ for pl_type in PL_TYPEs:
     pllist = {
         'payload_type': pl_type,
     } # Propose 'name' should be payload_type + autoincr, 'global_tag' default is not set yet
-    r = requests.post(url = url, json=pllist)
+    r = requests.post(url = url, headers=headers, json=pllist)
     datar = r.json()
     pll_names[pl_type] = datar['name']
     print(r.json())
@@ -82,7 +69,7 @@ for pl_type, pll_name in pll_names.items(): #pl_type not used in attach json, bu
     # check GT_NAME and pll_name exist
     # get payload_type of payload_list and if pl_type exists then this is a replace (update) operation
     # change 'global_tag' field in the payload_list object to GT_NAME (and to null for the replaced list)
-    r = requests.put(url = url, json=pl_attach)
+    r = requests.put(url = url, headers=headers, json=pl_attach)
     print(r.json())
 
 
@@ -91,7 +78,7 @@ print("Locking Global Tag")
 url = base_url + '/gt_change_status/'
 url = url + GT_NAME
 url = url + "/locked"
-r = requests.put(url = url)
+r = requests.put(url = url, headers=headers)
 print(r.json())
 
 #pll_names = {'ZDC': u'ZDC_309', 'Beam': u'Beam_306', 'CEMC_Geo': u'CEMC_Geo_310', 'CEMC_Thresh': u'CEMC_Thresh_308', 'FieldMap': u'FieldMap_307'}
@@ -110,11 +97,12 @@ for pl_type, pll_name in pll_names.items():
         'minor_iov': 1,
 #        'major_iov_end':  16,
 #        'minor_iov_end': 16,
+        'checksum': '5123609951b9f57c8b9652cb289c798da09c15954021f70bd6a1fc3ed019e4f5'
     }#  'payload_list' attribute is set later via a new attach method, same as for payload_lists-> global_tags
     # Need to return the id
     print(f'url = {url}')
     print(f'json = {piov}')
-    r = requests.post(url = url, json=piov)
+    r = requests.post(url = url, headers=headers, json=piov)
     print(f'r = {r}')
     print(r.json())
     piov_ids[pll_name] = r.json()['id']
@@ -127,5 +115,11 @@ for pll_name, piov_id in piov_ids.items():
         'payload_list' : pll_name,
         'piov_id'      : piov_id
     } # check pll_name and piov_id, check locked status for logic and decide if insert ok or not
-    r = requests.put(url = url, json=piov_attach)
+    r = requests.put(url = url, headers=headers, json=piov_attach)
     print(r.json())
+
+#Read back PayloadIOVs for the GT and IOVs
+print("Read back PayloadIOVs")
+url = base_url + f'/payloadiovs/?gtName={GT_NAME}&majorIOV=1&minorIOV=1'
+r = requests.get(url = url, headers=headers)
+print(r.json())
